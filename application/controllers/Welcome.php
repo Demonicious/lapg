ab946dc227c85901287d2d21da841a8f22f21063
Mudassar Islam <demoncious@gmail.com>
2019-07-15 01:00:11 +0500

    Pretty Usable At This Point

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,9 +4,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 	public function index() {
 		$data = array();
-		$data["page_title"] = "Home";
-		$data["page"] = "home";
 		$data["url"] = base_url();
-		$this->load->view('view', $data);	
+		$isLoggedIn = $this->UtilityModel->isLoggedIn();
+		if($isLoggedIn) {
+			redirect($data['url']."user", "refresh");
+		} else {
+			$data["page_title"] = "Home";
+			$data["page"] = "home";
+			$this->load->view('view', $data);	
+		}
 	}
 }
--- application/models/UtilityModel.php
+++ application/models/UtilityModel.php
@@ -11,7 +11,7 @@
 		    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
 		    $pass = array(); //remember to declare $pass as an array
 		    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-		    for ($i = 0; $i < 8; $i++) {
+		    for ($i = 0; $i < 20; $i++) {
 		        $n = rand(0, $alphaLength);
 		        $pass[] = $alphabet[$n];
 		    }
